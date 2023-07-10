913bf37f19a2501efc15a3f71e5ec366d0728164
lixin <1605670114@qq.com>
2023-07-10 09:22:44 +0800

    security config

--- src/main/java/com/lixin/springsecuritylearn/config/WebSecurityConfig.java
+++ src/main/java/com/lixin/springsecuritylearn/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -39,6 +40,9 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((requestMatcherRegistry) ->
                         requestMatcherRegistry.anyRequest().authenticated())
                 .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .cors()
                 .and()
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
